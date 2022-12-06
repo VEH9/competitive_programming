abc1887df04f8a89cd79acb2482d015d5c93765c
VEH9 <ivan.s2002.mail@gmail.com>
2022-12-06 13:47:24 +0500

    fix

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -5,76 +5,73 @@ using System.Threading.Tasks;
 
 namespace HW3
 {
-    public interface IStack<T>
-    {
-        void Push(T item);
-        bool TryPop(out T item);
-        int Count { get; }
-    }
-
-    public class LockFreeStack<T> : IStack<T>
-    {
-        private Node<T> root;
-        public int Count
+        public interface IStack<T>
         {
-            get 
+            void Push(T item);
+            bool TryPop(out T item);
+            int Count { get; }
+        }
+
+        public class LockFreeStack<T> : IStack<T>
+        {
+            private Node<T> root;
+
+            public int Count
             {
-                var count = 0;
-                var currentNode = root;
+                get
+                {
+                    if (root != null) return root.index;
+                    return 0;
+                }
+            }
+
+            private class Node<T>
+            {
+                public Node<T> Next;
+                public T Value;
+                public int index;
+
+                public Node(T value)
+                {
+                    Value = value;
+                    Next = null;
+                    index = 0;
+                }
+            }
+
+            public void Push(T item)
+            {
+                var spin = new SpinWait();
+                var node = new Node<T>(item);
                 while (true)
                 {
-                    if (currentNode is null)
-                        break;
-                    count++;
-                    currentNode = currentNode.Next;
+                    var head = root;
+                    node.Next = head;
+                    node.index = Count + 1;
+                    if (Interlocked.CompareExchange(ref root, node, head) == head)
+                        return;
+                    spin.SpinOnce();
                 }
-                return count;
             }
-        }
-        private class Node<T> 
-        { 
-            public Node<T> Next;
-            public T Value;
 
-            public Node(T value)
+            public bool TryPop(out T result)
             {
-                Value = value;
-                Next = null;
-            }
-        }
-
-        public void Push(T item)
-        {
-            var spin = new SpinWait();
-            var node = new Node<T>(item);
-            while (true)
-            {
-                var head = root;
-                node.Next = head;
-                if (Interlocked.CompareExchange(ref root, node, head) == head)
-                    return;
-                spin.SpinOnce();
-            }
-        }
-
-        public bool TryPop(out T result)
-        {
-            result = default(T);
-            var spin = new SpinWait();
-            Node<T> head;
-            while (true)
-            {
-                head = root;
-                if (head == null)
-                    return false;
-                if (Interlocked.CompareExchange(ref root, head.Next, head) == head)
+                result = default(T);
+                var spin = new SpinWait();
+                Node<T> head;
+                while (true)
                 {
-                    result = head.Value;
-                    return true;
+                    head = root;
+                    if (head == null)
+                        return false;
+                    if (Interlocked.CompareExchange(ref root, head.Next, head) == head)
+                    {
+                        result = head.Value;
+                        return true;
+                    }
+
+                    spin.SpinOnce();
                 }
-                spin.SpinOnce();
             }
         }
-    }
-    }
 }
