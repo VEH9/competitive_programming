c6d6def19638033bc109dc5328e31f24803c4c32
VEH9 <ivan.s2002.mail@gmail.com>
2022-12-13 12:20:24 +0500

    HW4

---
+++ HW4/CustomThreadPool/ThreadPools/DotNetThreadPoolWrapper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+using CustomThreadPool.ThreadPools;
+
+namespace CustomThreadPool
+{
+    public class DotNetThreadPoolWrapper : IThreadPool
+    {
+        private long processedTask = 0L;
+        
+        public void EnqueueAction(Action action)
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(delegate
+            {
+                action.Invoke();
+                Interlocked.Increment(ref processedTask);
+            }, null);
+        }
+
+        public long GetTasksProcessedCount() => processedTask;
+    }
+}
